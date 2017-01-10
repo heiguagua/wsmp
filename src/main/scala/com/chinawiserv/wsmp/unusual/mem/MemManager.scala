d3c757c3cbf2ff810d3dba0b3fec35c81889ad02
zhangaw <zhangaw@chinawiserv.com>
2017-01-10 12:51:15 +0800

    wsmp初始化

--- src/main/scala/com/chinawiserv/wsmp/unusual/mem/MemManager.scala
+++ src/main/scala/com/chinawiserv/wsmp/unusual/mem/MemManager.scala
@@ -1,10 +1,11 @@
 package com.chinawiserv.wsmp.unusual.mem
 
+import com.chinawiserv.wsmp.jedis.JedisClient
 import com.chinawiserv.wsmp.model.Cmd
 import com.chinawiserv.wsmp.operator.Operator
 import com.codahale.jerkson.Json
-
 import scala.collection.mutable.{HashMap, ListBuffer}
+import com.chinawiserv.wsmp.jedis.JedisClient.JedisExtended;
 
 /**
   * 内存数据管理
@@ -14,6 +15,19 @@ class MemManager {
   private val dataCount = 10;
   private val memMap = new HashMap[Int, ListBuffer[Mem]]();
   private val webMap = new HashMap[Int, Web]();
+
+  def initMem(): Unit = {
+    try {
+      val jedis = JedisClient.pool.getResource;
+      val keys = jedis.keys("wsmp*");
+      if (keys != null && !keys.isEmpty) {
+      }
+      jedis.close();
+    }
+    catch {
+      case e: Exception => e.printStackTrace();
+    }
+  }
 
   def saveData(cmd: Cmd): Boolean = {
     saveData(Operator.toMem(cmd));
@@ -29,10 +43,22 @@ class MemManager {
         }
         list += mem;
         memMap += (mem.id -> list);
+        this.saveToRedis(mem.id, Json.generate[Mem](mem));
         result = true;
       }
       return result;
     });
+  }
+
+  def saveToRedis(key: Int, element: String): Unit = {
+    try {
+      val jedis = JedisClient.pool.getResource;
+      jedis.addMsg("wsmp"+key, element);
+      jedis.close();
+    }
+    catch {
+      case e: Exception => e.printStackTrace();
+    }
   }
 
   def readData(id: Int): List[Mem] = {
