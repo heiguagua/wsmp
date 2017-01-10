5877a4051646f5469fe791808f6ddd60ae867283
zhangaw <zhangaw@chinawiserv.com>
2017-01-10 15:39:42 +0800

    wsmp初始化

--- src/main/scala/com/chinawiserv/wsmp/unusual/Unusual.scala
+++ src/main/scala/com/chinawiserv/wsmp/unusual/Unusual.scala
@@ -4,7 +4,7 @@ import com.chinawiserv.wsmp.handler.DataHandler
 import com.chinawiserv.wsmp.model.Cmd
 import com.chinawiserv.wsmp.thread.{CustomThreadFactory, ThreadPool}
 import com.chinawiserv.wsmp.unusual.mem.MemManager
-import scala.collection.mutable.ListBuffer
+import scala.collection.JavaConversions._;
 
 class Unusual extends DataHandler {
 
@@ -12,22 +12,16 @@ class Unusual extends DataHandler {
   private val memManager = new MemManager();
   private val executor = ThreadPool.newThreadPool(60, new CustomThreadFactory("UnusualExecutor-"));
 
+  def compute(cmds : java.util.List[Cmd]): Unit = {
+    compute(cmds.toList);
+  }
+
   def compute(cmds : List[Cmd]): Unit = {
     if (cmds != null && !cmds.isEmpty) {
       val list = cmds.sliding(tasksOfExecutor, tasksOfExecutor);
       list.foreach(x => {
         executor.execute(new UnusualExecutor(memManager, x));
       });
-    }
-  }
-
-  def compute(cmds : java.util.List[Cmd]): Unit = {
-    if (cmds != null && !cmds.isEmpty) {
-      val list = new ListBuffer[Cmd];
-      for (i <- 0.until(cmds.size())) {
-        list += cmds.get(i);
-      }
-      this.compute(list.toList);
     }
   }
 
