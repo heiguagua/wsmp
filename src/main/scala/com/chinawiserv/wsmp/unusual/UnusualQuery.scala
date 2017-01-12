99b0a6da6a17e1e7ffbdbc47127fef3cb3fef7dd
zhangaw <zhangaw@chinawiserv.com>
2017-01-12 20:02:37 +0800

    wsmp初始化

--- src/main/scala/com/chinawiserv/wsmp/unusual/UnusualQuery.scala
+++ src/main/scala/com/chinawiserv/wsmp/unusual/UnusualQuery.scala
@@ -12,24 +12,28 @@ import org.bson.conversions.Bson
 
 object UnusualQuery {
 
-  def main(args: Array[String]): Unit = {
-
-    val un = new CUnusualLevel(10);
-    /*
-    db.Unusual52010001.aggregate([
-        {$sort: {dt: -1}},
-        {$group: {_id: "$freq", amount: {$sum: 1}, level: {$first: "$level"}, dt: {$first: "$dt"}}},
-        {$sort: {_id: 1}}
-    ])
-          */
-
-    val pipeline: List[Bson] = new ArrayList[Bson];
+  def getUnusualById(id: String): List[Document] = {
+   // db.Unusual52010001.aggregate([
+   //   {$sort: {dt: -1}},
+   //   {$group: {_id: "$freq", amount: {$sum: 1}, level: {$first: "$level"}, dt: {$first: "$dt"}}},
+   //   {$sort: {_id: 1}}
+   //   ])
+    val pipeline = new ArrayList[Bson];
     pipeline.add(Aggregates.sort(new Document("dt", -1)));
-    pipeline.add(Aggregates.group(null, new BsonField("count", new Document("$sum", 1))))
+    pipeline.add(Aggregates.group(new Document("_id", "$freq"),
+                                  new BsonField("amount", new Document("$sum", 1)),
+                                  new BsonField("level", new Document("$first", "$level")),
+                                  new BsonField("dt", new Document("$first", "$dt"))))
+    pipeline.add(Aggregates.sort(new Document("_id", 1)));
+    MongoDB.mc.aggregate("wsmpExt", "Unusual"+id, pipeline);
+  }
 
-    //pipeline.add(Aggregates.project(new Document("_id", 0)))
-    val list: List[Document] = MongoDB.mc.aggregate("wsmpExt", "Unusual52010001", pipeline);
-    println(list);
+  def main(args: Array[String]): Unit = {
+    val list = getUnusualById("52010001");
+    for (i <- 0.until(list.size())) {
+      val doc = list.get(i);
+      println(doc);
+    }
   }
 
 }
