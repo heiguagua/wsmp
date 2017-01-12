9c97e29191f8f6894d0cec16ef944f5985320417
zhangaw <zhangaw@chinawiserv.com>
2017-01-12 19:02:48 +0800

    wsmp初始化

--- src/main/scala/com/chinawiserv/wsmp/unusual/UnusualQuery.scala
+++ src/main/scala/com/chinawiserv/wsmp/unusual/UnusualQuery.scala
@@ -1,6 +1,13 @@
 package com.chinawiserv.wsmp.unusual
 
+import java.util.List
+import java.util.ArrayList
+
+import com.chinawiserv.wsmp.mongodb.MongoDB
 import com.dgbc.caculate.CUnusualLevel
+import com.mongodb.client.model.{Aggregates, BsonField}
+import org.bson.Document
+import org.bson.conversions.Bson
 
 
 object UnusualQuery {
@@ -8,20 +15,21 @@ object UnusualQuery {
   def main(args: Array[String]): Unit = {
 
     val un = new CUnusualLevel(10);
-/*
+    /*
     db.Unusual52010001.aggregate([
-      {$sort: {dt: -1}},
-      {$group: {_id: "$freq", amount: {$sum: 1}, level: {$first: "$level"}, dt: {$first: "$dt"}}},
-      {$sort: {_id: 1}}
-      ])*/
+        {$sort: {dt: -1}},
+        {$group: {_id: "$freq", amount: {$sum: 1}, level: {$first: "$level"}, dt: {$first: "$dt"}}},
+        {$sort: {_id: 1}}
+    ])
+          */
 
-   /* val pipeline: util.List[Bson] = new util.ArrayList[Bson];
+    val pipeline: List[Bson] = new ArrayList[Bson];
     pipeline.add(Aggregates.sort(new Document("dt", -1)));
     pipeline.add(Aggregates.group(null, new BsonField("count", new Document("$sum", 1))))
 
     //pipeline.add(Aggregates.project(new Document("_id", 0)))
-    val list: util.List[Document] = MongoDB.mc.aggregate("wsmpExt", "Unusual52010001", pipeline);
-    println(list);*/
+    val list: List[Document] = MongoDB.mc.aggregate("wsmpExt", "Unusual52010001", pipeline);
+    println(list);
   }
 
 }
