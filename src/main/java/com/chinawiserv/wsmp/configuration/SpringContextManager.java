e27a770cc468c7b323760127538f4b86d90e6d4b
曾鹏志 <zengpzh@chinawiserv.com>
2017-01-13 20:31:40 +0800

    优化

---
+++ src/main/java/com/chinawiserv/wsmp/configuration/SpringContextManager.java
@@ -0,0 +1,29 @@
+package com.chinawiserv.wsmp.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2017/1/13.
+ */
+@Component
+public class SpringContextManager {
+
+    private static ApplicationContext applicationContext;
+
+    @Autowired
+    public void  setContxt(ApplicationContext context){
+        applicationContext = context;
+    }
+
+    public static <T> T getBean(Class<T> clazz){
+        return applicationContext.getBean(clazz);
+    }
+
+    @SuppressWarnings("Unchecked")
+    public static <T> T getBean(String name){
+        return (T)applicationContext.getBean(name);
+    }
+
+}
