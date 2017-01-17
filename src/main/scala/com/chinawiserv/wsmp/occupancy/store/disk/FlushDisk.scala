fff037becd58fbb9fe7bd0324c4f9477fb6d6112
曾鹏志 <zengpzh@chinawiserv.com>
2017-01-17 11:08:16 +0800

    优化

--- src/main/scala/com/chinawiserv/wsmp/occupancy/store/disk/FlushDisk.scala
+++ src/main/scala/com/chinawiserv/wsmp/occupancy/store/disk/FlushDisk.scala
@@ -1,28 +1,22 @@
 package com.chinawiserv.wsmp.occupancy
 package store.disk
 
+import java.util
 import java.util.concurrent.LinkedBlockingQueue
 
+import com.chinawiserv.wsmp._
 import com.chinawiserv.wsmp.occupancy.model.OccupancyData
 import com.chinawiserv.wsmp.thread.{CustomThreadFactory, ThreadPool}
+import com.mongodb.client.model._
+import org.apache.commons.lang.StringUtils
+import org.bson.Document
 import org.slf4j.{Logger, LoggerFactory}
+
+import scala.collection.JavaConversions
 
 /**
   * Created by zengpzh on 2017/1/6.
   */
-private class FlushDisk extends Runnable {
-
-  override def run() {
-    while (!Thread.currentThread().isInterrupted) {
-      try {
-        FlushDisk.flush;
-      } catch {
-        case e: Exception => Thread.currentThread().interrupt();
-      }
-    }
-  }
-
-}
 
 private[occupancy] object FlushDisk {
 
@@ -34,17 +28,57 @@ private[occupancy] object FlushDisk {
 
   private val flushDiskExecutorService = ThreadPool.newThreadPool(FLUSH_DISK_CONCURRENT_NUM, new CustomThreadFactory("Occupancy-Flush-Disk-Executor-"));
 
-  flushDiskExecutorService.execute(new FlushDisk());
+  flushDiskExecutorService.execute(new FlushDiskTask());
 
-  def offer(occupancyDatas: List[OccupancyData]): Unit ={
-    if(occupancyDatas != null && !occupancyDatas.isEmpty){
-      flushDiskQueue.offer(occupancyDatas);
+  def offer(records: List[OccupancyData]): Unit ={
+    if(records != null && !records.isEmpty){
+      flushDiskQueue.offer(records);
     }
   }
 
-  private def flush: Unit = {
+  private[disk] def flush: Unit = {
     logger.info("Occupancy flush DISK, queued: {} ", flushDiskQueue.size);
-    flushDiskExecutorService.execute(new FlushDiskTask(FlushDisk.flushDiskQueue.take));
+    val records = flushDiskQueue.take;
+    flushDiskExecutorService.execute(new Runnable {
+      override def run(): Unit = {
+        val now = System.currentTimeMillis();
+        doFlush(records);
+        logger.info("Occupancy flush DISK, execute time: {} {}", System.currentTimeMillis() - now, "MS");
+      }
+    })
+  }
+
+  private def doFlush(records: List[OccupancyData]): Unit = {
+    if (records != null && !records.isEmpty) {
+      try {
+        val writeModels = new util.ArrayList[WriteModel[Document]]();
+        var collection = "";
+        records.foreach(record => {
+          val station = record.id;
+          val time = record.time;
+          val maxLevels = JavaConversions.asJavaCollection[Byte](record.levels);
+          if (StringUtils.isNotBlank(time) && time.length == (TIME_YEAR_LENGTH + TIME_DAY_LENGTH) && maxLevels != null) {
+            if (collection.isEmpty) {
+              collection = collection_prefix + time.take(TIME_YEAR_LENGTH);
+            }
+            mongoDB.shardCollection(collection, new Document("station", 1));
+            val daytime = time.takeRight(TIME_DAY_LENGTH);
+            val filter = Filters.and(Filters.eq("station", station), Filters.eq("time", daytime));
+            val replacement = new Document("station", station)
+              .append("time", daytime)
+              .append("maxLevels", maxLevels);
+            val replaceOneModel = new ReplaceOneModel[Document](filter, replacement, new UpdateOptions().upsert(true));
+            writeModels.add(replaceOneModel);
+          }
+        });
+        if (writeModels.size > 0 && collection.length == (collection_prefix.length + TIME_DAY_LENGTH)) {
+          mongoDB.mc.bulkWrite(mongoDB.dbName, collection, writeModels, new BulkWriteOptions().ordered(false));
+          writeModels.clear();
+        }
+      } catch {
+        case e: Exception => e.printStackTrace();
+      }
+    }
   }
 
 }
--- src/main/scala/com/chinawiserv/wsmp/occupancy/store/disk/FlushDiskTask.scala
+++ src/main/scala/com/chinawiserv/wsmp/occupancy/store/disk/FlushDiskTask.scala
@@ -2,64 +2,17 @@ package com.chinawiserv.wsmp
 package occupancy
 package store.disk
 
-import java.util
-
-import com.chinawiserv.wsmp.occupancy.model.OccupancyData
-import com.mongodb.client.model._
-import org.apache.commons.lang.StringUtils
-import org.bson.Document
-import org.slf4j.{Logger, LoggerFactory}
-
-import scala.collection.JavaConversions
-
 /**
   * Created by zengpzh on 2017/1/6.
   */
-private class FlushDiskTask(records: List[OccupancyData]) extends Runnable {
-
-  private val logger: Logger = LoggerFactory.getLogger(classOf[FlushDiskTask]);
+private class FlushDiskTask extends Runnable {
 
   override def run(): Unit = {
-    if (records != null) {
-      val now = System.currentTimeMillis();
-      FlushDiskTask.flushRecords(records);
-      logger.info("Occupancy flush DISK, execute time: {} {}", System.currentTimeMillis() - now, "MS");
-    }
-  }
-
-}
-
-private[disk] object FlushDiskTask {
-
-  def flushRecords(records: List[OccupancyData]): Unit = {
-    if (records != null && !records.isEmpty) {
+    while (!Thread.currentThread().isInterrupted) {
       try {
-        val writeModels = new util.ArrayList[WriteModel[Document]]();
-        var collection = "";
-        records.foreach(record => {
-          val station = record.id;
-          val time = record.time;
-          val maxLevels = JavaConversions.asJavaCollection[Byte](record.levels);
-          if (StringUtils.isNotBlank(time) && time.length == (TIME_YEAR_LENGTH + TIME_DAY_LENGTH) && maxLevels != null) {
-            if (collection.isEmpty) {
-              collection = collection_prefix + time.take(TIME_YEAR_LENGTH);
-            }
-            mongoDB.shardCollection(collection, new Document("station", 1));
-            val daytime = time.takeRight(TIME_DAY_LENGTH);
-            val filter = Filters.and(Filters.eq("station", station), Filters.eq("time", daytime));
-            val replacement = new Document("station", station)
-              .append("time", daytime)
-              .append("maxLevels", maxLevels);
-            val replaceOneModel = new ReplaceOneModel[Document](filter, replacement, new UpdateOptions().upsert(true));
-            writeModels.add(replaceOneModel);
-          }
-        });
-        if (writeModels.size > 0 && collection.length == (collection_prefix.length + TIME_DAY_LENGTH)) {
-          mongoDB.mc.bulkWrite(mongoDB.dbName, collection, writeModels, new BulkWriteOptions().ordered(false));
-          writeModels.clear();
-        }
+        FlushDisk.flush;
       } catch {
-        case e: Exception => e.printStackTrace();
+        case e: Exception => Thread.currentThread().interrupt();
       }
     }
   }
