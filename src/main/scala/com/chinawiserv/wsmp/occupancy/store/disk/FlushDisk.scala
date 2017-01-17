a2a187f8fc7db1284db688b82212ca7c47f582db
曾鹏志 <zengpzh@chinawiserv.com>
2017-01-17 10:36:49 +0800

    优化

--- src/main/scala/com/chinawiserv/wsmp/occupancy/store/disk/FlushDisk.scala
+++ src/main/scala/com/chinawiserv/wsmp/occupancy/store/disk/FlushDisk.scala
@@ -30,7 +30,7 @@ private[occupancy] object FlushDisk {
 
   private val flushDiskQueue = new LinkedBlockingQueue[List[OccupancyData]]();
 
-  private val FLUSH_DISK_CONCURRENT_NUM: Int = 5;
+  private val FLUSH_DISK_CONCURRENT_NUM: Int = 3;
 
   private val flushDiskExecutorService = ThreadPool.newThreadPool(FLUSH_DISK_CONCURRENT_NUM, new CustomThreadFactory("Occupancy-Flush-Disk-Executor-"));
 
