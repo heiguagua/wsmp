23c78d9a7227e6517a5aa0e872fe83ea10a267d4
hanqk <hanqk@chinawiserv.com>
2017-06-06 11:18:06 +0800

    项目前期构建
    1.删除scala 代码
    2.添加web项目目录

--- src/main/scala/com/chinawiserv/wsmp/occupancy/model/OccupancyData.scala
+++
@@ -1,6 +0,0 @@
-package com.chinawiserv.wsmp.occupancy.model
-
-/**
-  * Created by zengpzh on 2017/1/11.
-  */
-private[occupancy] case class OccupancyData(id: Int, time: String, levels: Array[Byte])
