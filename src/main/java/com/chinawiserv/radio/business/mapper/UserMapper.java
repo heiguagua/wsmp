416eb3c0b569e74294d8f7d25e08b7ef8fc2efda
wuhaoran <wuhaoran@192.168.9.158>
2017-06-06 17:43:55 +0800

    代码提交

--- src/main/java/com/chinawiserv/radio/business/mapper/UserMapper.java
+++ src/main/java/com/chinawiserv/radio/business/mapper/UserMapper.java
@@ -1,14 +1,11 @@
 package com.chinawiserv.radio.business.mapper;
 
 import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
-
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.baomidou.mybatisplus.plugins.pagination.Pagination;
 import com.chinawiserv.radio.business.pojo.User;
 
-@Mapper
+
 public interface UserMapper extends BaseMapper<User> {
 
 	List<User> selectUserList(Pagination page, String state);
--- src/main/java/com/chinawiserv/radio/config/mybaitis/MybatisPlusConfig.java
+++ src/main/java/com/chinawiserv/radio/config/mybaitis/MybatisPlusConfig.java
@@ -1,24 +1,16 @@
 package com.chinawiserv.radio.config.mybaitis;
 
-import java.io.IOException;
-
-import javax.sql.DataSource;
-import org.apache.ibatis.io.DefaultVFS;
-import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.spring.MybatisMapperRefresh;
-import com.baomidou.mybatisplus.spring.MybatisSqlSessionFactoryBean;
 
 @Configuration
+@MapperScan("com.chinawiserv.radio.business.mapper")
 public class MybatisPlusConfig {
-
-	@Autowired
-	private DataSource dataSource;
 
 	@Autowired
 	ApplicationContext context;
@@ -26,41 +18,8 @@ public class MybatisPlusConfig {
 	/* 开发时可以打开这个配置用于实时刷新mapper*,打包的时候关闭减少性能消耗 */
 	@Bean
 	public MybatisMapperRefresh getMybatisMapperRefresh() throws Exception {
-		MybatisSqlSessionFactoryBean sqlSessionFactoryBean = this.context.getBean(MybatisSqlSessionFactoryBean.class);
-		MybatisMapperRefresh refresh = new MybatisMapperRefresh(sqlSessionFactoryBean.getObject(),1,1,true);
+		SqlSessionFactory sqlSessionFactory = this.context.getBean(SqlSessionFactory.class);
+		MybatisMapperRefresh refresh = new MybatisMapperRefresh(sqlSessionFactory,1,1,true);
 		return refresh;
-	}
-
-	/**
-	 * myBaits 配置
-	 * 
-	 * @return
-	 * @throws IOException
-	 */
-	@Bean
-	public MybatisSqlSessionFactoryBean getSqlSessionFactoryBean() throws IOException {
-		MybatisSqlSessionFactoryBean sqlSessionFactoryBean = new MybatisSqlSessionFactoryBean();
-		sqlSessionFactoryBean.setMapperLocations(this.context.getResources("classpath:mapper/*/*.xml"));
-		sqlSessionFactoryBean.setDataSource(this.dataSource);
-		sqlSessionFactoryBean.setVfs(DefaultVFS.class);
-		return sqlSessionFactoryBean;
-	}
-
-	@Bean
-	public MapperScannerConfigurer initMapperScannerConfigurer() {
-		MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
-		mapperScannerConfigurer.setBasePackage("com.chinawiserv.radio.business.mapper");
-		// mapperScannerConfigurer.setAnnotationClass(Repository.class);
-		return mapperScannerConfigurer;
-	}
-
-	/**
-	 * mybatis-plus分页插件
-	 */
-	@Bean
-	public PaginationInterceptor paginationInterceptor() {
-		PaginationInterceptor page = new PaginationInterceptor();
-		page.setDialectType("mysql");
-		return page;
 	}
 }
