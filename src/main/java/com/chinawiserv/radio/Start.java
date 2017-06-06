2d5aa4ac9495efc0a0517cfeb6a477ad6990f9ae
wuhaoran <wuhaoran@192.168.9.158>
2017-06-06 18:36:39 +0800

    代码提交

--- src/main/java/com/chinawiserv/radio/Start.java
+++ src/main/java/com/chinawiserv/radio/Start.java
@@ -1,12 +1,11 @@
 package com.chinawiserv.radio;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import com.chinawiserv.radio.business.mapper.UserMapper;
 
 @SpringBootApplication
+@MapperScan("com.chinawiserv.radio.business.mapper")
 public class Start {
 
 	public static void main(String[] args) {
