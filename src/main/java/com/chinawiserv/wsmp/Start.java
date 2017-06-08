2d224a27644c3cb9c0e89704adb8863053f6f009
吴浩然 <wuhaoran@MacBook-Air.local>
2017-06-08 14:49:21 +0800

    代码提交

--- src/main/java/com/chinawiserv/wsmp/Start.java
+++ src/main/java/com/chinawiserv/wsmp/Start.java
@@ -1,11 +1,11 @@
 package com.chinawiserv.wsmp;
 
-import org.mybatis.spring.annotation.MapperScan;
+//import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-@MapperScan("com.chinawiserv.radio.business.mapper")
+//@SpringBootApplication
+//@MapperScan("com.chinawiserv.radio.business.mapper")
 public class Start {
 
 	public static void main(String[] args) {
