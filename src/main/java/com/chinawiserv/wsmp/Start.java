b4c0ed10b6f84c0366ffe7838002df114af32e33
hanqk <hanqk@chinawiserv.com>
2017-06-26 13:14:20 +0800

    合并启动类和Tomcat支持启动类

--- src/main/java/com/chinawiserv/wsmp/Start.java
+++ src/main/java/com/chinawiserv/wsmp/Start.java
@@ -3,10 +3,17 @@ package com.chinawiserv.wsmp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.chinawiserv.radio.business.mapper")
-public class Start {
+public class Start extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TestStart.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestStart.class);
