9520ee6d08a73a5bef30b7fc2dcb7f024811221b
吴浩然 <wuhaoran@MacBook-Air.local>
2017-06-26 20:55:21 +0800

    代码提交

--- src/main/java/com/chinawiserv/wsmp/cache/TableColumnFactory.java
+++ src/main/java/com/chinawiserv/wsmp/cache/TableColumnFactory.java
@@ -1,32 +1,75 @@
 package com.chinawiserv.wsmp.cache;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Properties;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.EncodedResource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.stereotype.Component;
 
+import com.chinawiserv.wsmp.pojo.AlarmDealed;
+import com.chinawiserv.wsmp.pojo.AlarmUnDealed;
 import com.chinawiserv.wsmp.pojo.BandStatusTable;
 
 @Component
 public class TableColumnFactory {
-	
-	@Cacheable(value = "BandStatus", sync = true)
-	public static BandStatusTable getBandStatusTable() {
-		DefaultResourceLoader loader = new DefaultResourceLoader();
 
-		try {
-			Properties p = PropertiesLoaderUtils.loadProperties(loader.getResource("table_column/band_status.properties"));
-			String radioName = p.getProperty("radio_name");
-			System.out.println();
-		}
-		catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
+	// 频段状态表头
+	@Cacheable(value = "BandStatus", sync = true)
+	public BandStatusTable getBandStatusTable() throws IOException {
+
+		DefaultResourceLoader loader = new DefaultResourceLoader();
+		final Resource resource = loader.getResource("table_column/band_status.properties");
+		EncodedResource encodedResource = new EncodedResource(resource, Charset.forName("utf-8"));
+		Properties p = PropertiesLoaderUtils.loadProperties(encodedResource);
+		String radioName = p.getProperty("radio_name", "频段名称");
+		String legalStation = p.getProperty("legal_station", "合法正常台站");
+		String illegalStation = p.getProperty("illegal_station", "合法违规台站");
+		String legalSignal = p.getProperty("legal_signal", "已知信号");
+		String illegalSignal = p.getProperty("illegal_signal", "不明信号");
+		String unknownSignal = p.getProperty("unknown_signal", "非法信号");
+		return new BandStatusTable(radioName, legalStation, illegalStation, legalSignal, unknownSignal, illegalSignal);
+
+	}
+
+	// 实时警告处理表头
+	@Cacheable(value = "AlarmDealed", sync = true)
+	public AlarmDealed getAlarmDealed() throws IOException {
+		DefaultResourceLoader loader = new DefaultResourceLoader();
+		final Resource resource = loader.getResource("table_column/alarm_dealed.properties");
+		EncodedResource encodedResource = new EncodedResource(resource, Charset.forName("utf-8"));
+		Properties p = PropertiesLoaderUtils.loadProperties(encodedResource);
+		String radio = p.getProperty("radio", "频率");
+		String firstTime = p.getProperty("first_appear_time", "首次出现时间");
+		String lastingTime = p.getProperty("lasting_time", "持续时间");
+		String radioType = p.getProperty("radio_type", "类型");
+		String station = p.getProperty("station", "监测站");
+		String radioStatus = p.getProperty("radio_status", "状态");
+		String mark = p.getProperty("mark", "备注");
+		return new AlarmDealed(radio, firstTime, lastingTime, radioType, station, radioStatus, mark);
+
+	}
+
+	// 实时警告未处理表头
+	@Cacheable(value = "AlarmUnDealed", sync = true)
+	public AlarmUnDealed getAlarmUnDealed() throws IOException {
+		DefaultResourceLoader loader = new DefaultResourceLoader();
+		final Resource resource = loader.getResource("table_column/alarm_undealed.properties");
+		EncodedResource encodedResource = new EncodedResource(resource, Charset.forName("utf-8"));
+		Properties p = PropertiesLoaderUtils.loadProperties(encodedResource);
+		String radio = p.getProperty("radio", "频率");
+		String firstTime = p.getProperty("first_appear_time", "首次出现时间");
+		String lastingTime = p.getProperty("lasting_time", "持续时间");
+		String radioType = p.getProperty("radio_type", "类型");
+		String station = p.getProperty("station", "监测站");
+		String radioStatus = p.getProperty("radio_status", "状态");
+		String mark = p.getProperty("mark", "备注");
+		return new AlarmUnDealed(radio, firstTime, lastingTime, radioType, station, radioStatus, mark);
+
 	}
 
 }
--- src/main/java/com/chinawiserv/wsmp/pojo/BandStatusTable.java
+++ src/main/java/com/chinawiserv/wsmp/pojo/BandStatusTable.java
@@ -1,76 +1,78 @@
 package com.chinawiserv.wsmp.pojo;
 
-
 public class BandStatusTable {
 
-	private String tabletype;
+	private String radioName = "";
 
-	private String bandName;
+	private String legalStation = "";
 
-	private String legalNormalStation;
+	private String illegalStation = "";
 
-	private String legalUnnormalStation;
+	private String legalSignal = "";
 
-	private String unKnownSignal;
+	private String unknownSignal = "";
 
-	private String knownSignal;
+	private String illegalSignal = "";
 
-	private String illegalSignal;
+	public BandStatusTable() {}
 
-	public String getBandName() {
-		return bandName;
-	}
 
-	public void setBandName(String bandName) {
-		this.bandName = bandName;
-	}
-
-	public String getLegalNormalStation() {
-		return legalNormalStation;
-	}
-
-	public void setLegalNormalStation(String legalNormalStation) {
-		this.legalNormalStation = legalNormalStation;
-	}
-
-	public String getLegalUnnormalStation() {
-		return legalUnnormalStation;
-	}
-
-	public void setLegalUnnormalStation(String legalUnnormalStation) {
-		this.legalUnnormalStation = legalUnnormalStation;
-	}
-
-	public String getUnKnownSignal() {
-		return unKnownSignal;
-	}
-
-	public void setUnKnownSignal(String unKnownSignal) {
-		this.unKnownSignal = unKnownSignal;
-	}
-
-	public String getKnownSignal() {
-		return knownSignal;
-	}
-
-	public void setKnownSignal(String knownSignal) {
-		this.knownSignal = knownSignal;
-	}
-
-	public String getIllegalSignal() {
-		return illegalSignal;
-	}
-
-	public void setIllegalSignal(String illegalSignal) {
+	public BandStatusTable(String radioName, String legalStation, String illegalStation, String legalSignal, String unknownSignal, String illegalSignal) {
+		super();
+		this.radioName = radioName;
+		this.legalStation = legalStation;
+		this.illegalStation = illegalStation;
+		this.legalSignal = legalSignal;
+		this.unknownSignal = unknownSignal;
 		this.illegalSignal = illegalSignal;
 	}
 
-	public String getTabletype() {
-		return tabletype;
+	public String getRadio_name() {
+		return radioName;
 	}
 
-	public void setTabletype(String tabletype) {
-		this.tabletype = tabletype;
+	public void setRadio_name(String radio_name) {
+		this.radioName = radio_name;
+	}
+
+	public String getLegal_station() {
+		return legalStation;
+	}
+
+	public void setLegalStation(String legal_station) {
+		this.legalStation = legal_station;
+	}
+
+	public String getIllegal_station() {
+		return illegalStation;
+	}
+
+	public void setIllegalStation(String illegal_station) {
+		this.illegalStation = illegal_station;
+	}
+
+	public String getLegal_signal() {
+		return legalSignal;
+	}
+
+	public void setLegal_signal(String legal_signal) {
+		this.legalSignal = legal_signal;
+	}
+
+	public String getUnknown_signal() {
+		return unknownSignal;
+	}
+
+	public void setUnknown_signal(String unknown_signal) {
+		this.unknownSignal = unknown_signal;
+	}
+
+	public String getIllegal_signal() {
+		return illegalSignal;
+	}
+
+	public void setIllegal_signal(String illegal_signal) {
+		this.illegalSignal = illegal_signal;
 	}
 
 }
