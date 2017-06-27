37c16fa7556f8b2a791711281d368fbc39ada664
吴浩然 <wuhaoran@MacBook-Air.local>
2017-06-27 19:57:14 +0800

    代码提交

--- src/main/java/com/chinawiserv/wsmp/Start.java
+++ src/main/java/com/chinawiserv/wsmp/Start.java
@@ -5,11 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
-@MapperScan("com.chinawiserv.radio.business.mapper")
+@MapperScan("com.chinawiserv.wsmp.mapper")
 public class Start extends SpringBootServletInitializer {
 
 	@Override
