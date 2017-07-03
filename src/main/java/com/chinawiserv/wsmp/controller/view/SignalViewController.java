c24084ebc7025c7ce072e5619507ecea41f23266
吴浩然 <wuhaoran@MacBook-Air.local>
2017-07-03 17:22:59 +0800

    代码提交

--- src/main/java/com/chinawiserv/wsmp/controller/view/SignalViewController.java
+++ src/main/java/com/chinawiserv/wsmp/controller/view/SignalViewController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.chinawiserv.wsmp.pojo.Signature;
 
 @Controller
-@RequestMapping("signal")
+@RequestMapping("/signal")
 public class SignalViewController {
 
     @RequestMapping(path = {"/", ""})
