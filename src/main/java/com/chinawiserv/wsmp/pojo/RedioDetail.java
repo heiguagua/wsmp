7e66bce929505d47d97b4f1208587464abebae65
吴浩然 <wuhaoran@MacBook-Air.local>
2017-07-03 15:02:34 +0800

    代码提交

---
+++ src/main/java/com/chinawiserv/wsmp/pojo/RedioDetail.java
@@ -0,0 +1,35 @@
+package com.chinawiserv.wsmp.pojo;
+
+
+public class RedioDetail {
+
+	private String name;
+
+	private float centor;
+
+	private float band;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getBand() {
+		return band;
+	}
+
+	public void setBand(float band) {
+		this.band = band;
+	}
+
+	public float getCentor() {
+		return centor;
+	}
+
+	public void setCentor(float centor) {
+		this.centor = centor;
+	}
+}
