b654ff30a0ff421879c0fc8c9036d89c0aa70869
吴浩然 <wuhaoran@MacBook-Air.local>
2017-07-06 11:56:45 +0800

    代码提交

---
+++ src/main/java/com/chinawiserv/wsmp/pojo/Area.java
@@ -0,0 +1,26 @@
+package com.chinawiserv.wsmp.pojo;
+
+
+public class Area {
+
+	private String name;
+
+	private String areaCode;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAreaCode() {
+		return areaCode;
+	}
+
+	public void setAreaCode(String areaCode) {
+		this.areaCode = areaCode;
+	}
+
+}
