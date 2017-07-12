91cbb3000271ab62630297297105f64a3852e0f0
吴浩然 <wuhaoran@MacBook-Air.local>
2017-07-12 10:39:56 +0800

    代码提交

--- src/main/java/com/chinawiserv/wsmp/Start.java
+++ src/main/java/com/chinawiserv/wsmp/Start.java
@@ -2,13 +2,16 @@ package com.chinawiserv.wsmp;
 
 import java.io.FileNotFoundException;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-//@SpringBootApplication
-//@MapperScan("com.chinawiserv.wsmp.mapper")
-//@EnableAsync
+@SpringBootApplication
+@MapperScan("com.chinawiserv.wsmp.mapper")
+@EnableAsync
 public class Start extends SpringBootServletInitializer {
 
 	@Override
