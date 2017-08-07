b4983fb3ad7f8312d0580fc0bdb89208650d38a3
chenht <chenht@chinawiserv.com>
2017-08-07 16:08:08 +0800

    1

--- src/main/java/com/chinawiserv/wsmp/controller/data/ASIQDataController.java
+++ src/main/java/com/chinawiserv/wsmp/controller/data/ASIQDataController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chinawiserv.wsmp.hbase.HbaseClient;
+
 import com.chinawiserv.wsmp.hbase.query.AudioData;
 import com.google.common.net.HttpHeaders;
 
