dd191b1524d50018f52a7da9b0d699e41a9ad4ad
wuhr <wuhr@chinawiserv.com>
2017-08-25 15:31:17 +0800

    代码提交

--- src/main/java/com/chinawiserv/wsmp/controller/data/SiganlDataController.java
+++ src/main/java/com/chinawiserv/wsmp/controller/data/SiganlDataController.java
@@ -306,12 +306,12 @@ public class SiganlDataController {
 			reslute.put("id","");
 		}else{
 			reslute.put("id",id);
+			reslute.put("saveDate",historyDTO.getSaveDate().toString().replace("T"," "));
+			reslute.put("historyType",historyDTO.getHistoryType());
+			reslute.put("invalidDate",historyDTO.getInvalidDate().toString().replace("T"," "));
+			reslute.put("des",historyDTO.getDes());
 		}
-
-		reslute.put("saveDate",historyDTO.getSaveDate().toString().replace("T"," "));
-		reslute.put("historyType",historyDTO.getHistoryType());
-		reslute.put("invalidDate",historyDTO.getInvalidDate().toString().replace("T"," "));
-		reslute.put("des",historyDTO.getDes());
+		
 		return  reslute;
 	}
 
