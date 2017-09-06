9107c41c223980d8d7459f45e101cde6e2abb0d5
dengtl <dengtl@chinawiserv.com>
2017-09-06 17:30:35 +0800

    BUG #45140

--- src/main/webapp/3.9/js/dojo/home/alarm/day_levelcharts.js
+++ src/main/webapp/3.9/js/dojo/home/alarm/day_levelcharts.js
@@ -29,7 +29,7 @@ define([ "ajax", "echarts", "jquery" ], function(ajax, echarts, jquery) {
                 }],
                 grid : {
                     left : '1%',
-                    right : '4%',
+                    right : '2%',
                     bottom : '2%',
                     top : 30,
                     containLabel : true
@@ -39,10 +39,10 @@ define([ "ajax", "echarts", "jquery" ], function(ajax, echarts, jquery) {
                 },
                 xAxis : {
                     type : 'category',
-                    name:'信号频率(MHz)',
-                    nameRotate:'-90',
-                    nameLocation:'end',
-                    boundaryGap : false,
+                    //name:'信号频率(MHz)',
+                    //nameRotate:'-90',
+                    //nameLocation:'end',
+                    //boundaryGap : false,
                     axisLine : {
                         lineStyle : {
                             color : '#DAE5F0'
